c319393fa48b559f2ab7fca6b750b5fb98695e67
Jean-Louis Blervacque <louis121b44@gmail.com>
2022-03-27 20:32:26 +0200

    Update index.js

--- Server_Nodejs/index.js
+++ Server_Nodejs/index.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 8080 },()=>{
     console.log('Neurosity server started')
 })
-
+let i = 0;
 const spinner = require("ora")().start();
 spinner.info("Navigate to https://console.neurosity.co/ to access training data.");
 
@@ -12,15 +12,47 @@ wss.on('connection', function connection(ws) {
 (async () => {
   console.log('client connected')
   const mind = await connect();
-  spinner.start(`Hand open`);
+  ws.send("open");
+  isOpen = true;
   
+  spinner.start(`Think about your right index finger`);
+  function waiting(){
+    console.log("\nopening...")
+    ws.send("open");
+    isOpen = true;
+  }
+  /*setTimeout(test, 2000);
+  
+  function test(){
+    console.log("\n...Test..." + i);
+    ws.send(i);
+    i++;
+    setTimeout(test, 2000);
+  }
+  ws.send("TEST");*/
+  let intervalId = undefined;
   mind.kinesis("rightIndexFinger").subscribe(async (intent) => {
     //console.log(intent);
     if (intent.confidence >= 0.7&&intent.confidence < 0.99) {
-      ws.send("close");
-      //console.log("\nHand closed")
-      await wait(2000);
-      ws.send("open");
+      console.log("\nintent");
+      if(isOpen){
+        ws.send("close");
+        console.log("\nclosing...")
+      }
+
+      isOpen = false;
+      if(intervalId) {
+        try {
+          clearInterval(intervalId);
+        } catch {}
+      }
+      intervalId = setTimeout(waiting,3000);
+      
+      
+      
+      
+      
+      
     
       //spinner.text = `Command detected. Awaiting confirmation=(conf=${intent.confidence})`;
       
